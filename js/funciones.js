1b6f1ff3a8fe154fe5a365ea88c6f055c8b10723
diego <diebox@gmail.com>
2017-12-09 17:23:35 -0300

    Creación del repositorio

---
+++ js/funciones.js
@@ -0,0 +1,22 @@
+$(document).on('ready', function() {
+  $(".regular").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 1
+  });
+
+	$(document).click(function() {
+	  $(".resultados-busqueda").removeClass("visible");
+	  $(".resultados-busqueda").addClass("oculto");
+	  $(".buscador-home input").removeClass("no-radius");	
+	});
+
+	$(".buscador-home").click(function(event) {
+	  $(".resultados-busqueda").addClass("visible");
+	  $(".resultados-busqueda").removeClass("oculto");
+	  $(".buscador-home input").addClass("no-radius");	
+	  return false;
+	}); 
+
+});
